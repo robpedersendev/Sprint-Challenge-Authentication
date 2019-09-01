6d7403744c618b29b4b614d8012f521cb95fdc3b
robpedersendev <rob84044@gmail.com>
2019-08-31 19:07:44 -0600

    Strucutured the authouter file

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
+router.post("/register", async (req, res) => {
   // implement registration
+  const user = req.body;
+  try {
+    if (!body) {
+      res.status(404).json({ message: "No body provided" });
+    } else {
+    }
+  } catch (error) {}
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
